84fc38337b9761d6e098a40a92860486f2b0f337
Alexis Algoud <alexis@atm-consulting.fr>
2015-04-15 12:15:10 +0200

    init

---
+++ admin/jouroff_setup.php
@@ -0,0 +1,244 @@
+<?php
+    require('../config.php');
+    
+    dol_include_once('/jouroff/class/jouroff.class.php');
+    
+    if(dol_include_once('/absence/class/absence.class.php')) {
+        $emploiTemps=new TRH_EmploiTemps;
+        dol_include_once('/absence/lib/absence.lib.php');    
+    }
+    else {
+        $emploiTemps=null;
+    }
+    
+    
+    $langs->load('jouroff@jouroff');
+    $langs->load('absence@absence');
+    
+    $PDOdb=new TPDOdb;
+    
+    $feries=new TRH_JoursFeries;
+    
+    if(isset($_REQUEST['action'])) {
+        switch($_REQUEST['action']) {
+            case 'sync':
+                
+                if(!empty($conf->global->MAIN_INFO_SOCIETE_COUNTRY)) {
+                    list($id_country, $code_country) = explode(':', $conf->global->MAIN_INFO_SOCIETE_COUNTRY);
+                    
+                    if($code_country=='FR') {
+                        $url='https://www.google.com/calendar/ical/fr.french%23holiday%40group.v.calendar.google.com/public/basic.ics';
+                    }
+                    else{
+                        $url = '';  
+                    }
+                    
+                }
+                
+                
+                if(!empty($conf->global->ABSENCE_SYNC_CALENDAR)) {
+                    $url = $conf->global->ABSENCE_SYNC_CALENDAR;
+                }
+                
+                if(empty($url)) {
+                    setEventMessage($langs->trans('ErrCalendarURLNotFound'), 'errors');
+                }
+                else{
+                    TRH_JoursFeries::syncronizeFromURL($PDOdb, $url);   
+                }
+                
+                
+                _liste($PDOdb, $feries , $emploiTemps);
+                
+                break;
+            case 'add':
+            case 'new':
+                //$PDOdb->db->debug=true;
+                $feries->set_values($_REQUEST);
+                _fiche($PDOdb, $feries,$emploiTemps, 'edit');
+                break;  
+            case 'edit' :
+                $feries->load($PDOdb, $_REQUEST['idJour']);
+                _fiche($PDOdb, $feries,$emploiTemps,'edit');
+                break;
+                
+            case 'save':
+                $feries->load($PDOdb, $_REQUEST['idJour']); 
+                //print_r($feries);     
+                $feries->set_values($_REQUEST);
+                
+                $existeDeja=$feries->testExisteDeja($PDOdb);
+                
+                if(!$existeDeja){
+                    $feries->save($PDOdb);
+                    _liste($PDOdb, $feries , $emploiTemps);
+                }else{
+                    $mesg = '<div class="error">' . $langs->trans('ErrNoWorkedDayAlreadyExist') . '</div>';
+                    _fiche($PDOdb, $feries , $emploiTemps,'edit');
+                }
+                
+                
+                break;
+            
+            case 'view':
+                $feries->load($PDOdb, $_REQUEST['idJour']);
+                
+                _fiche($PDOdb, $feries,$emploiTemps,'view');
+                
+                
+                break;
+            case 'delete':
+                //$PDOdb->db->debug=true;
+                $feries->load($PDOdb, $_REQUEST['idJour']);
+                $feries->delete($PDOdb, $_REQUEST['idJour']);
+                $mesg = '<div class="ok">' . $langs->trans('DayDeleted') . '</div>';
+                $mode = 'edit';
+                _liste($PDOdb, $feries , $emploiTemps);
+                break;
+        }
+    }
+    elseif(isset($_REQUEST['id'])) {
+        _liste($PDOdb, $feries , $emploiTemps);
+        
+                
+    }
+    else {
+        //$PDOdb->db->debug=true;
+        _liste($PDOdb, $feries, $emploiTemps);
+    }
+    
+    
+    $PDOdb->close();
+    
+    llxFooter();
+    
+    
+function _liste(&$PDOdb, $feries, $emploiTemps ) {
+    global $langs, $conf, $db, $user;   
+    llxHeader('', $langs->trans('ListOfAbsence'));
+    
+    if(!is_null($emploiTemps))  print dol_get_fiche_head(edtPrepareHead($emploiTemps, 'emploitemps')  , 'joursferies', $langs->trans('Absence'));
+    
+    //getStandartJS();  
+    
+    $r = new TSSRenderControler($feries);
+    $sql="SELECT rowid as 'ID', date_cre as 'DateCre', 
+             date_jourOff, moment as 'Période',  commentaire as 'Commentaire', '' as 'Supprimer'
+        FROM  ".MAIN_DB_PREFIX."rh_absence_jours_feries
+        WHERE entity IN (0,".$conf->entity.")";
+        
+    
+    $TOrder = array('date_jourOff'=>'DESC');
+    if(isset($_REQUEST['orderDown']))$TOrder = array($_REQUEST['orderDown']=>'DESC');
+    if(isset($_REQUEST['orderUp']))$TOrder = array($_REQUEST['orderUp']=>'ASC');
+                
+    $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;           
+    //print $page;
+    $form=new TFormCore($_SERVER['PHP_SELF'],'formtranslateList','GET');
+    
+    $r->liste($PDOdb, $sql, array(
+        'limit'=>array(
+            'page'=>$page
+            ,'nbLine'=>'30'
+        )
+        ,'link'=>array(
+            'date_jourOff'=>'<a href="?idJour=@ID@&fk_user='.$user->id.'&action=view">@val@</a>'
+            ,'Supprimer'=>$user->rights->absence->myactions->ajoutJourOff?"<a onclick=\"if (window.confirm('" . $langs->trans('DoYouReallyWantDeletePublicHoliday') . "')){href='?idJour=@ID@&fk_user=".$user->id."&action=delete'};\"><img src='./img/delete.png'></a>":''
+        ) 
+        ,'translate'=>array(
+            'Période'=>array('matin'=> $langs->trans('AbsenceMorning'),'apresmidi'=> $langs->trans('AbsenceAfternoon'),'allday'=> $langs->trans('AbsenceAllDay'))
+        )
+        ,'hide'=>array('DateCre')
+        ,'type'=>array('date_jourOff'=>'date')
+        ,'liste'=>array(
+            'titre'=> $langs->trans('PublicHolidayNonWorkedDaysList')
+            ,'image'=>img_picto('','title.png', '', 0)
+            ,'picto_precedent'=>img_picto('','previous.png', '', 0)
+            ,'picto_suivant'=>img_picto('','next.png', '', 0)
+            ,'noheader'=> (int)isset($_REQUEST['socid'])
+            ,'messageNothing'=>"Aucun jour non travaillé"
+            ,'order_down'=>img_picto('','1downarrow.png', '', 0)
+            ,'order_up'=>img_picto('','1uparrow.png', '', 0)
+            ,'picto_search'=>'<img src="../../theme/rh/img/search.png">'
+            
+        )//theme/rh/img/search.png
+        ,'title'=>array(
+            'date_jourOff'=> $langs->trans('NoWorkedDays')
+        )
+        ,'search'=>array(
+            'date_jourOff'=>array('recherche'=>'calendar')
+            
+        )
+        ,'orderBy'=>$TOrder
+        
+    ));
+    if($user->rights->absence->myactions->ajoutJourOff=="1"){
+        ?>
+        <a class="butAction" href="?fk_user=<?=$user->id?>&action=new"><?php echo $langs->trans('New'); ?></a>
+        &nbsp;
+        <a class="butAction" href="?action=sync"><?php echo $langs->trans('OnlineSynchronization'); ?></a>
+        <div style="clear:both"></div>
+        <?php
+    }
+    $form->end();
+    
+    llxFooter();
+}   
+    
+function _fiche(&$PDOdb, $feries, $emploiTemps, $mode) {
+    global $db,$user,$idUserCompt, $idComptEnCours, $langs;
+    llxHeader('', $langs->trans('Schedule'));
+    
+    $form=new TFormCore($_SERVER['PHP_SELF'],'form1','POST');
+    $form->Set_typeaff($mode);
+    echo $form->hidden('idJour', $feries->getId());
+    echo $form->hidden('action', 'save');
+    echo $form->hidden('id', $user->id);
+
+    
+    $TBS=new TTemplateTBS();
+    print $TBS->render('./tpl/joursferies.tpl.php'
+        ,array(
+            
+        )
+        ,array(
+            'joursFeries'=>array(
+                'id'=>$feries->getId()
+                ,'date_jourOff'=>$form->calendrier('', 'date_jourOff', $feries->date_jourOff, 12)
+                ,'moment'=>$form->combo('','moment',$feries->TMoment,$feries->moment)
+                ,'commentaire'=>$form->zonetexte('','commentaire',$feries->commentaire, 40,3,'','','-')
+                ,'titreCreate'=>load_fiche_titre($langs->trans('NewPublicHoliday'),'', 'title.png', 0, '')
+                ,'titreAction'=>$_GET['action']
+                ,'titreVisu'=>load_fiche_titre($langs->trans('PublicHolidayVisualization'),'', 'title.png', 0, '')
+            )
+            ,'userCourant'=>array(
+                'id'=>$user->id
+                ,'droitAjoutJour'=>$user->rights->absence->myactions->ajoutJourOff
+            )
+            ,'view'=>array(
+                'mode'=>$mode
+                ,'head'=>(!is_null($emploiTemps)) ? dol_get_fiche_head(edtPrepareHead($emploiTemps, 'emploitemps')  , 'joursferies', $langs->trans('Absence')) : ''
+            )
+            ,'translate' => array(
+                'NoWorkedDays' => $langs->trans('NoWorkedDays'),
+                'Period' => $langs->trans('Period'),
+                'Comment' => $langs->trans('Comment'),
+                'ConfirmDeletePublicHoliday' => $langs->trans('ConfirmDeletePublicHoliday'),
+                'Delete' => $langs->trans('Delete'),
+                'Modify' => $langs->trans('Modify'),
+                'Back' => $langs->trans('Back'),
+                'Register' => $langs->trans('Register'),
+                'Cancel' => $langs->trans('Cancel')
+            )
+        )   
+    );
+    
+    echo $form->end_form();
+    // End of page
+    
+    global $mesg, $error;
+    dol_htmloutput_mesg($mesg, '', ($error ? 'error' : 'ok'));
+    llxFooter();
+} 
+
+
---
+++ class/jouroff.class.php
@@ -0,0 +1,133 @@
+<?php 
+
+class TRH_JoursFeries extends TObjetStd {
+    function __construct() {
+        global $langs;
+         
+        parent::set_table(MAIN_DB_PREFIX.'rh_absence_jours_feries');
+        parent::add_champs('date_jourOff','type=date;index;');
+        parent::add_champs('moment','type=chaine;index;');
+        parent::add_champs('commentaire','type=chaine;');
+        parent::add_champs('entity','type=entier;index;');
+        
+        
+        parent::_init_vars();
+        parent::start();    
+        
+        $this->TFerie=array();
+        $this->TMoment=array(
+            'allday'=> $langs->trans('AbsenceAllDay'),
+            'matin'=> $langs->trans('AbsenceMorning'),
+            'apresmidi'=> $langs->trans('AbsenceAfternoon')
+        );
+        
+        $this->moment = 'allday';       
+    }
+    
+    
+    function save(&$db) {
+        global $conf;
+        $this->entity = $conf->entity;
+        
+        if(!$this->testExisteDeja($db)) {
+            parent::save($db);  
+        }
+
+    }
+
+    
+    //fonction qui renvoie 1 si le jour férié que l'on veut créer existe déjà à la date souhaitée, sinon 0
+    function testExisteDeja(&$ATMdb){
+        global $conf;
+        //on récupère toutes les dates de jours fériés existant
+        $sql="SELECT count(*) as 'nb'  FROM ".MAIN_DB_PREFIX."rh_absence_jours_feries
+             WHERE date_jourOff='".$this->get_date('date_jourOff','Y-m-d')."' AND rowid!=".$this->getId();
+        $ATMdb->Execute($sql);
+        $obj = $ATMdb->Get_line();
+            
+        //on teste si l'un d'eux est égal à celui que l'on veut créer
+        if($obj->nb > 0){
+            return 1;   
+        }
+        
+        return 0;
+    }
+    
+    static function estFerie(&$ATMdb, $date) {
+        global $conf;
+        //on récupère toutes les dates de jours fériés existant
+        $sql="SELECT count(*) as 'nb'  FROM ".MAIN_DB_PREFIX."rh_absence_jours_feries
+             WHERE entity IN (0,".(! empty($conf->multicompany->enabled) && ! empty($conf->multicompany->transverse_mode)?"1,":"").$conf->entity.")
+             AND  date_jourOff=".$ATMdb->quote($date);
+             
+        $ATMdb->Execute($sql);
+        $obj = $ATMdb->Get_line();
+            
+        //on teste si l'un d'eux est égal à celui que l'on veut créer
+        if($obj->nb > 0){
+            return true;    
+        }
+        
+        return false;
+        
+        
+    }
+    
+    static function syncronizeFromURL(&$ATMdb, $url) {
+        
+        $iCal = new ICalReader( $url );
+        
+        foreach($iCal->cal['VEVENT'] as $event) {
+        
+            if($event['STATUS']=='CONFIRMED') {
+                
+                $jf = new TRH_JoursFeries;
+                $jf->commentaire = $event['SUMMARY'];
+                
+                $aaaa = substr($event['DTSTART'], 0,4);
+                $mm = substr($event['DTSTART'], 4,2);
+                $jj = substr($event['DTSTART'], 6,2);
+                
+                $jf->set_date('date_jourOff', $jj.'/'.$mm.'/'.$aaaa);
+                
+                $jf->save($ATMdb);
+                
+            }
+
+
+        }
+        
+    }
+    
+    
+    static function getAll(&$ATMdb, $date_start='', $date_end='') {
+        global $conf;   
+        
+        $Tab=array();
+              //récupération des jours fériés 
+        $sql2=" SELECT moment,commentaire,date_jourOff,rowid FROM  ".MAIN_DB_PREFIX."rh_absence_jours_feries
+         WHERE entity IN (0,".(! empty($conf->multicompany->enabled) && ! empty($conf->multicompany->transverse_mode)?"1,":"").$conf->entity.")";
+         
+        if(!empty($date_start) && !empty($date_end)) $sql2.="AND date_jourOff BETWEEN ".$ATMdb->quote($date_start)." AND ".$ATMdb->quote($date_end);
+         
+        
+        $ATMdb->Execute($sql2);
+        
+         while ($row = $ATMdb->Get_line()) {
+             $Tab[] =$row;
+          
+         }
+        
+        return $Tab;
+    
+    }
+    
+}
+
+class TJourOff extends TRH_JoursFeries {
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+}
